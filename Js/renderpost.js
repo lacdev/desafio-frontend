450e1d42a4a9be36adb14f65d67e96328e6ea70e
S3len3Ch4vz <nasaya.369@gmail.com>
2021-11-08 09:04:45 -0600

    Finalización correcta del update

--- Js/renderpost.js
+++ Js/renderpost.js
@@ -20,7 +20,6 @@ $tags = document.querySelector(".tags"),
 $titulo = document.querySelector(".titulo"),
 $contenido = document.querySelector(".contenido-post"),
 $editar = document.querySelector("modificar"),
-//$template = document.querySelector(".imprimir"),
 //Para no hacer varias incerciones al DOM asi que se abre el fragmento
 $fragment = document.createDocumentFragment();
 
@@ -28,10 +27,10 @@ $fragment = document.createDocumentFragment();
 
 const crud = (metodos) => {
     //Destructuración para el objeto 
-    /* Url para llamar a la db 
-    method para llamar a (get,post,put o delete)
-    sucess y error identificar el estado de respuesta (200 - 400)
-    data para saber si se envia datos o no
+    /* Url - para llamar a la db 
+    method  - para llamar a (get,post,put o delete)
+    sucess y error - identificar el estado de respuesta (200 - 400)
+    data - para saber si se envia datos o no
     */
     let { url, method, succes, error, data } = metodos;
     const request = new XMLHttpRequest();
@@ -74,17 +73,23 @@ const getPost = () => {
 //Función de lo que cada selector de cada elemento de html se tomara de la bd
 //Respuesta .- Indica cada elemento de la bd se a igual al selector de cada querySelector por clase. 
 const renderPost = (respuesta) => {
+    //Selectores para el modal que se muestren sus valores
+    let coverimagen = document.querySelector("#imagen-url")
+    coverimagen.value = respuesta.imageURL
+    let titlecontent = document.querySelector("#title-post")
+    titlecontent.value = respuesta.title
+    let contentpost = document.querySelector("#contenido-post")
+    contentpost.value = respuesta.contenido
+
 
     $autor.textContent = respuesta.name
     $fecha.textContent = respuesta.fecha
     $titulo.textContent = respuesta.title
     $contenido.textContent = respuesta.contenido
     $imagen.src = respuesta.imageURL
-   
-    //Los tags serán insertados en el html siendo leidos como cadena
-    //
+   // console.log("Esto es respuesta", respuesta);
     $tags.innerHTML = "";
-    const children = $tags.respuesta.childNodes;
+    const children = respuesta.tags;
       console.log(children);
     children.forEach((tag) => {
         const pTag = document.createElement("p");
@@ -104,28 +109,7 @@ document.addEventListener("DOMContentLoaded", getPost);
 2. Abrir el form o el modul para hacer la edición 
 3. Pintar el modul con los datos a transformar 
 4. Guardar los cambios. 
-
 */
-/*const putPost = () => {
-    let idfake = "-MniCq-1zu_afiH_LwPg";
-    crud({
-        url:`https://desafio-js-fa573-default-rtdb.firebaseio.com/${idfake}.json`,
-        method: "PUT",
-        succes: (respuesta) => { 
-            renderPost(respuesta)  },
-        error: (err) => {
-            console.log(err)   },
-        data:{
-            name: $autor.value,
-            title: $titulo.value,
-            fecha: $fecha.value,
-            imageURL: $imagen.value,
-            contenido: $contenido.value,
-        }
-    })
-}
-
-/* --------------------------------- Prueba --------------------------------- */
 
 
 
@@ -141,17 +125,28 @@ document.getElementById("salvar").addEventListener("click", event => {
     let  date = new Date();
     let $fecha = `${date.getFullYear()}-${date.getMonth() +1 }-${date.getDate()}`;
    // console.log("🚀 ~ file: renderpost.js ~ line 131 ~ document.getElementById ~ $contenido", $contenido)
+   /* Array - sirve para obtenerlo como objeto de este tipo y no como HTMLCollection 
+      childNodes - con el fin de traerme lo que se crea apartir del selector tags que son los parrafos de cada 
+      tag
+      map - para acceder a la propiedad de texto de cada tag
+   */
+   
+   let $tags = Array.from(document.querySelector(".tags").childNodes).map((htmlTag) => {
+     return htmlTag.innerText;
+   })
+   console.log("🚀 ~ file: renderpost.js ~ line 145 ~ document.getElementById ~ $tags", $tags)
+   //console.log($tags)
     const postId = urlparameter("id")
     crud ({
         url:`https://desafio-js-fa573-default-rtdb.firebaseio.com/${postId}.json`,
-        method: "PUT",
+        method: "PATCH",
         succes: (respuesta) => { renderPost ({ 
             name: $autor.textContent,
             title: $titulo,
            imageURL: $imagen,
           contenido: $contenido,
            fecha: $fecha,
-            tags: $tags 
+           tags:  $tags
         })},
         error: (err) => $form.insertAdjacentHTML("afterend, <p><b>${err}</b></p>"),
         data: {   
@@ -160,7 +155,7 @@ document.getElementById("salvar").addEventListener("click", event => {
            imageURL: $imagen,
           contenido: $contenido,
            fecha: $fecha,
-            tags: $tags
+           tags: $tags
             }
         })
 
@@ -174,25 +169,3 @@ const urlparameter =  (paramName) => {
    return param; 
 }
   
-/* ------------------------------------ X ----------------------------------- 
-document.addEventListener('button', event => {
-    let idfake = "-MniCq-1zu_afiH_LwPg";
-//Identificar si esta con o sin datos 
-    if(event.target.id.value){      
-        //UPDATE- PUT
-        crud ({
-            url:`https://desafio-js-fa573-default-rtdb.firebaseio.com/${idfake}.json`,
-            method: "PUT",
-            succes: (respuesta) => location.reload,
-            error: (err) => $form.insertAdjacentHTML("afterend, <p><b>${err}</b></p>"),
-            data: {
-                name: event.target.author.value,
-                title: event.target.titulopost.value,
-                imageURL: event.target.fondoimagen.value,
-                contenido: event.target.info.value
-                }
-            })
-    }
-
-})
-*/
